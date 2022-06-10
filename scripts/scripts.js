8f584357ead216e36f8d7cc30f3edb06844c604d
Victoria Martynenko <105857981+v-victoria@users.noreply.github.com>
2022-06-10 21:24:41 +0100

    Fixed navbar jump

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,9 +1,14 @@
 let navbarElem = document.querySelector(".navbar");
+let navbarSizeReplaceElem = document.querySelector(".navbar-size-replace");
 
 window.addEventListener("scroll", function (e) {
   if (this.scrollY > window.innerHeight) {
     navbarElem.classList.add("fixed-top");
+    navbarSizeReplaceElem.classList.remove("d-none");
+    navbarSizeReplaceElem.classList.add("d-block");
   } else {
     navbarElem.classList.remove("fixed-top");
+    navbarSizeReplaceElem.classList.add("d-none");
+    navbarSizeReplaceElem.classList.remove("d-block");
   }
 });
